8a60eddf1648ea8f654d71f95e0b78ba098748c2
Vladislav Elcheninov <133252067+vvladislovv@users.noreply.github.com>
2025-11-19 12:30:54 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const repoBasePath = process.env.NEXT_PUBLIC_BASE_PATH
+  ? `/${process.env.NEXT_PUBLIC_BASE_PATH.replace(/^\/+|\/+$/g, '')}`
+  : ''
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  basePath: repoBasePath,
+  assetPrefix: repoBasePath ? `${repoBasePath}/` : undefined,
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true,
